f023f4c99cfc9d9b5a298c0a92c02c6dfb113e89
KetchuppOfficial <dolgov04@list.ru>
2023-11-16 14:55:59 +0300

    Scaling of nodes fixed

--- libviews/controlflowgraphview.cpp
+++ libviews/controlflowgraphview.cpp
@@ -2443,8 +2443,7 @@ void ControlFlowGraphView::parseNode(QTextStream& lineStream)
     {
         assert(node->instrNumber() > 0);
         node->setVisible(true);
-
-        qreal w = _scaleX * nodeWidth.toDouble();
+        qreal w = (_scaleX - 6) * nodeWidth.toDouble();
         qreal h = _scaleY * nodeHeight.toDouble();
 
         auto rItem = new CanvasCFGNode{this, node, xx - w / 2, yy - h / 2, w, h};
